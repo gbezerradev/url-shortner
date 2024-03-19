9f310273894f7483ae135fdb1c6dab64d73cca95
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2024-03-19 00:04:36 -0300

    update API endpoint and add status route

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,9 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(() => {
+    router.get('/', async () => {
+      return { status: 'ok' }
+    })
+  })
+  .prefix('api/v1')
