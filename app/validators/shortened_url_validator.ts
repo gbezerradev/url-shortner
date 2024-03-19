1177a7dd71f212f409189287c04c27036d59d1c6
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2024-03-19 10:57:13 -0300

    Refactor shortenedUrl validation to be optional

--- app/validators/shortened_url_validator.ts
+++ app/validators/shortened_url_validator.ts
@@ -3,6 +3,6 @@ import vine from '@vinejs/vine'
 export const createShortenedUrl = vine.compile(
   vine.object({
     originalUrl: vine.string().url({ protocols: ['http', 'https'], require_protocol: true }),
-    shortenedUrl: vine.string().alphaNumeric().minLength(3),
+    shortenedUrl: vine.string().alphaNumeric().minLength(3).optional(),
   })
 )
